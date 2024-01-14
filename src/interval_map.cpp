d54807208a5c9eaaf8bca3b657b4b8abecd5bb0d
Tran Minh-Hung <minhhung.1301@gmail.com>
2024-01-14 22:50:40 +0700

    f2

--- src/interval_map.cpp
+++ src/interval_map.cpp
@@ -1,18 +1,38 @@
+#include <iostream>
+
 #include <map>
+
 template <typename K, typename V> class interval_map {
   friend void IntervalMapTest();
-  V m_valBegin; //holds the value that is associated with all keys less than the first key in m_map. 
+  V m_valBegin; // holds the value that is associated with all keys less than
+                // the first key in m_map.
   std::map<K, V> m_map;
 
 public:
   // constructor associates whole range of K with val
   interval_map(V const &val) : m_valBegin(val) {}
 
+  void toString() {
+    std::cout << std::endl << std::endl << std::endl;
+    for (auto it = m_map.begin(); it != m_map.cend(); ++it) {
+      std::cout << "interval_map key _" << it->first << "_val_" << it->second
+                << std::endl;
+    }
+    std::cout << std::endl << std::endl << std::endl;
+  }
+
   bool isKeyEqual(K key1, K key2) {
     if (!(key1 < key2) && !(key2 < key1)) {
       return true;
     }
     return false;
+  }
+
+  K getMinKey(K key1, K key2) {
+    if (key1 < key2) {
+      return key1;
+    }
+    return key2;
   }
 
   // Assign value val to interval [keyBegin, keyEnd).
@@ -27,44 +47,62 @@ public:
       return;
     }
 
-    if (m_map.size()== 0) {
+    if (m_map.size() == 0) {
       m_map.insert(std::pair{keyBegin, val});
 
       m_map.insert(std::pair{keyEnd, m_valBegin});
     }
 
-    auto upperBoud = m_map.upper_boud(keyBegin);
+    auto upperBound = m_map.upper_bound(keyEnd);
 
-    if (upperBoud == m_map.cend()) {
-      if (isKeyEqual(upperBoud->first, keyBegin) 
-       || !(keyBegin < m_map.cend()->first)) {
-          m_map.insert(m_map.cend(), std::pair{keyEnd, upperBoud->second});
-          m_map.erase(m_map.cend());
-          upperBoud = m_map.cend();
+    if (upperBound == m_map.cend()) {
+      if (upperBound->first < keyEnd) {
+        m_map.insert(m_map.cend(), std::pair{keyEnd, upperBound->second});
+        m_map.erase(m_map.cend());
+        upperBound = m_map.cend();
       }
     }
 
-    auto lowerBoud = upperBoud;
+    auto lowerBound = upperBound;
+    auto nearestUpperBoundVal = m_valBegin;
+    if (m_map.size() != 1) {
+      nearestUpperBoundVal = (upperBound - 1)->second;
+    }
+
     while (true) {
-      if (lowerBoud == m_map.cbegin()) {
-        if (lowerBoud->first < keyBegin) {
-          lowerBoudKey = lowerBoud -> first;
-        } 
+      if (lowerBound == m_map.cbegin() || lowerBound->first < keyBegin) {
         break;
       }
-      lowerBoud--;
+      lowerBound--;
     }
 
-    m_map.erase(lowerBoud, upperBoud);
-    m_map.insert(upperBoud, std::pair(keyBegin, val));
-    if (keyEnd < upperBoud->first) {
-      m_map.insert(upperBoud, std::pair(keyEnd, lowerBoud->second));
+    if (m_map.size() == 1) {
+      if (upperBound->second == val) {
+        m_map.insert(upperBound, std::pair(keyBegin, val));
+        m_map.erase(m_map.cend());
+      }
+    } else {
+      int insertedElement = 0;
+      if (!(lowerBound->second == val)) {
+        m_map.insert(upperBound, std::pair{keyBegin, val});
+        insertedElement++;
+      }
+
+      if (!(nearestUpperBoundVal == val)) {
+        m_map.insert(upperBound, std::pair{keyEnd, nearestUpperBoundVal});
+        insertedElement++;
+      }
+      if (lowerBound->first < keyBegin) {
+        m_map.erase(lowerBound + 1, upperBound - insertedElement);
+      } else {
+        m_map.erase(lowerBound, upperBound - insertedElement);
+      }
     }
   }
 
   // look-up of the value associated with key
   V const &operator[](K const &key) const {
-    auto it = m_map.upper_boud(key);
+    auto it = m_map.upper_Bound(key);
     if (it == m_map.begin()) {
       return m_valBegin;
     } else {
@@ -77,11 +115,19 @@ public:
 // to discover the cases that your implementation does not handle correctly.
 // We recommend to implement a test function that tests the functionality of
 // the interval_map, for example using a map of int intervals to char.
-
-
-#include <iostream>
 int main() {
   std::cout << std::endl << std::endl << std::endl << std::endl << std::endl;
-  std::cout << "hello aaaa aaaa ";
+  interval_map<int, char> map = interval_map('h');
+  map.assign(-5, 0, 'a');
+  map.toString();
+  map.assign(0, 5, 'b');
+  map.toString();
+  map.assign(10, 15, 'd');
+  map.toString();
+  map.assign(15, 20, 'e');
+  map.toString();
+  map.assign(5, 10, 'c');
+  map.toString();
+  map.assign(-10, -5, 'A');
   return 0;
 }
