95d5e530916746ed871deb2599e5250e399d6177
Tran Minh-Hung <minhhung.1301@gmail.com>
2024-06-28 18:07:11 +0700

    leetcode.com/problems/h-index

--- src/main.cpp
+++ src/main.cpp
@@ -6,54 +6,22 @@ using namespace std;
 
 class Solution {
 public:
-  int jump(vector<int> &nums) {
-    if (nums.size() == 1) {
-      return 0;
-    }
-    queue<int> bfs_queue = {};
-
-    vector<int> tracing(nums.size(), -1);
-    bfs_queue.push(0);
-    tracing[0] = -1;
-
-    while (!bfs_queue.empty()) {
-      int current_index = bfs_queue.front();
-      bfs_queue.pop();
-
-      int end_index = (int)nums.size() - 1;
-      if (current_index < end_index &&
-          nums.at(current_index) + current_index < end_index) {
-        end_index = nums.at(current_index) + current_index;
-      } else {
-        tracing[end_index] = current_index;
-        break;
-      }
-
-      for (int next_index = current_index + 1; next_index <= end_index;
-           next_index++) {
-        if (tracing[next_index] >= 0) {
-          continue;
+  int hIndex(vector<int> &citations) {
+    const int MAX_CITAIONS = citations.size();
+    int h_index = 0;
+    for (int citation_check = 1; citation_check <= MAX_CITAIONS;
+         citation_check++) {
+      int num_valid_paper = 0;
+      for (int i = 0; i < MAX_CITAIONS; i++) {
+        if (citations.at(i) >= citation_check) {
+          num_valid_paper++;
         }
-        bfs_queue.push(next_index);
-        tracing[next_index] = current_index;
+      }
+      if (num_valid_paper >= citation_check) {
+        h_index = citation_check;
       }
     }
-
-    int min_jump = 0;
-    int index = (int)nums.size() - 1;
-    do {
-
-      if (index >= 0) {
-        index = tracing[index];
-
-        min_jump++;
-      } else {
-        break;
-      }
-    } while (index != 0);
-
-
-    return min_jump;
+    return h_index;
   }
 };
 
@@ -67,24 +35,11 @@ template <typename T> void print_vector(vector<T> v) {
 int main() {
   Solution solution;
   cout << "***** hello ******" << endl;
-  vector<vector<int>> numx = {{2, 3, 1, 1, 4},
-                              {3, 2, 1, 0, 4},
-                              {0, 0, 0, 0, 0},
-                              {1},
-                              {0},
-                              {9},
-                              {0, 1},
-                              {1, 0},
-                              {9, 1, 0},
-                              {9, 0, 0, 0},
-                              {0, 1, 2},
-                              {1, 2},
-                              {1, 1, 1, 1, 1},
-                              {1, 2, 3, 4, 5},
-                              {5, 4, 3, 2, 1},
-                              {2, 1, 1, 1, 1}};
+  vector<vector<int>> numx = {
+      {3, 0, 6, 1, 5}, {1, 3, 1}, {3},    {0},    {1},    {2},
+      {1, 2},          {1, 1},    {9, 9}, {0, 0}, {0, 1}, {0, 10}};
   for (auto nums : numx) {
-    cout << solution.jump(nums) << "____";
+    cout << solution.hIndex(nums) << "____";
     print_vector(nums);
   }
 
